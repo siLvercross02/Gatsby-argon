3f5a809efbb4a6378e290e56f9575fb02ce35c07
Joemari Supan <joemari.supan18@gmail.com>
2021-11-28 21:42:32 +0800

    Update Contact us

--- src/components/pageComponent/contactUs/Banner.js
+++ src/components/pageComponent/contactUs/Banner.js
@@ -6,8 +6,11 @@ import {
     Card,
     CardBody,
 } from 'reactstrap'
+import { Form, Input, Button } from 'antd'
 
 export default function Banner() {
+    const { TextArea } = Input;
+
     return (
         <Fragment>
             <div className="position-relative">
@@ -27,6 +30,63 @@ export default function Banner() {
                                 </Col>
                             </Row>
                         </div>
+                    </Container>
+                </section>
+                <section className="section section-lg mt--200">
+                    <Container>
+                        <Row className="justify-content-center">
+                            <Col lg="12">
+                                <Card className="shadow border-0">
+                                    <CardBody className="py-5">
+                                        <div className="icon icon-shape icon-shape-primary rounded-circle mb-4">
+                                            <i className="ni ni-email-83" />
+                                        </div>
+                                        <h3 className="text-primary text-uppercase">
+                                            Send us a message
+                                        </h3>
+                                        <Form>
+                                            <Row>
+                                                <Col lg="6" md="6">
+                                                    <Form.Item>
+                                                        <Input placeholder="Your First Name" />
+                                                    </Form.Item>
+                                                </Col>
+                                                <Col lg="6" md="6">
+                                                    <Form.Item>
+                                                        <Input placeholder="Your Last" />
+                                                    </Form.Item>
+                                                </Col>
+                                            </Row>
+                                            <Row>
+                                                <Col lg="6" md="6">
+                                                    <Form.Item>
+                                                        <Input placeholder="Your Email Address" />
+                                                    </Form.Item>
+                                                </Col>
+                                                <Col lg="6" md="6">
+                                                    <Form.Item>
+                                                        <Input placeholder="Phone Number" />
+                                                    </Form.Item>
+                                                </Col>
+                                            </Row>
+                                            <Row justify="center">
+                                                <Col lg="12" md="12">
+                                                    <Form.Item>
+                                                        <TextArea rows={4} placeholder="Your Message" />
+                                                    </Form.Item>
+                                                    
+                                                </Col>
+                                                <Col lg="12" md="12">
+                                                    <div className="mx-auto text-center">
+                                                        <Button type="primary" htmlType="submit" className="pl-5 pr-5" size="large">Submit</Button>
+                                                    </div>
+                                                </Col>
+                                            </Row>
+                                        </Form>
+                                    </CardBody>
+                                </Card>
+                            </Col>
+                        </Row>
                     </Container>
                 </section>
             </div>
